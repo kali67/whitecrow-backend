047002e0f9a93af311369e5be50a89ae1a4a3202
Hayden Taylor <hta55@uclive.ac.nz>
2019-09-18 21:40:36 +1200

    fixed issue with not being able to update language

--- src/main/java/dto/UserDto.kt
+++ src/main/java/dto/UserDto.kt
@@ -3,4 +3,4 @@ package whitecrow.dto
 import whitecrow.model.*
 import java.util.*
 
-data class UserDto(val username: String?, val languageCode: LanguageCode?, val id: UUID, val hasCompletedPreTest: Boolean?)
+data class UserDto(val username: String?, val languageCode: LanguageCode?, val id: UUID?, val hasCompletedPreTest: Boolean?)
